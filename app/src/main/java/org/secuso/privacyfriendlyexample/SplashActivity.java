5432d8530f88178f43f2815db6bde1bd369112b0
Karola Marky <yonjuni@pinyto.de>
2016-10-22 13:36:40 +0300

    Splash Screen added

---
+++ app/src/main/java/org/secuso/privacyfriendlyexample/SplashActivity.java
@@ -0,0 +1,22 @@
+package org.secuso.privacyfriendlyexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karo on 22.10.16.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
