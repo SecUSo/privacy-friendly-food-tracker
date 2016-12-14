8162f91ddab54727a7c50fce64a3dbfb1ecb94a8
Karola Marky <yonjuni@pinyto.de>
2016-12-15 00:30:30 +0200

    Welcome dialog replaced by tutorial

--- app/src/main/java/org/secuso/privacyfriendlyexample/activities/MainActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyexample/activities/MainActivity.java
@@ -5,12 +5,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 import org.secuso.privacyfriendlyexample.R;
+import org.secuso.privacyfriendlyexample.tutorial.PrefManager;
+import org.secuso.privacyfriendlyexample.tutorial.TutorialActivity;
 
 public class MainActivity extends BaseActivity {
 
@@ -25,8 +28,13 @@ public class MainActivity extends BaseActivity {
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    WelcomeDialog welcomeDialog = new WelcomeDialog();
-                    welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
+//                    WelcomeDialog welcomeDialog = new WelcomeDialog();
+//                    welcomeDialog.show(getFragmentManager(), "WelcomeDialog");
+                    PrefManager prefManager = new PrefManager(getBaseContext());
+                    prefManager.setFirstTimeLaunch(true);
+                    Intent intent = new Intent(MainActivity.this, TutorialActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
                 }
             });
         }
@@ -40,32 +48,32 @@ public class MainActivity extends BaseActivity {
     }
 
 
-    public static class WelcomeDialog extends DialogFragment {
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-            LayoutInflater i = getActivity().getLayoutInflater();
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setView(i.inflate(R.layout.welcome_dialog, null));
-            builder.setIcon(R.mipmap.icon);
-            builder.setTitle(getActivity().getString(R.string.welcome));
-            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
-            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ((MainActivity)getActivity()).goToNavigationItem(R.id.nav_help);
-                }
-            });
-
-            return builder.create();
-        }
-    }
+//    public static class WelcomeDialog extends DialogFragment {
+//
+//        @Override
+//        public void onAttach(Activity activity) {
+//            super.onAttach(activity);
+//        }
+//
+//        @Override
+//        public Dialog onCreateDialog(Bundle savedInstanceState) {
+//
+//            LayoutInflater i = getActivity().getLayoutInflater();
+//            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//            builder.setView(i.inflate(R.layout.welcome_dialog, null));
+//            builder.setIcon(R.mipmap.icon);
+//            builder.setTitle(getActivity().getString(R.string.welcome));
+//            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
+//            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    ((MainActivity)getActivity()).goToNavigationItem(R.id.nav_help);
+//                }
+//            });
+//
+//            return builder.create();
+//        }
+//    }
 
     public void onClick(View view) {
         switch(view.getId()) {
