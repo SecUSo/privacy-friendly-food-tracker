9ff89fe8155eadd991ab6438deb08096da944832
sreinkemeier <sreinkemeier@inovex.de>
2019-01-22 16:00:00 +0100

    Added new Activities and fragments

---
+++ app/src/main/java/org/secuso/privacyfriendlyfoodtracker/activities/BaseAddFoodActivity.java
@@ -0,0 +1,56 @@
+package org.secuso.privacyfriendlyfoodtracker.activities;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.secuso.privacyfriendlyfoodtracker.R;
+import org.secuso.privacyfriendlyfoodtracker.activities.adapter.AddFoodPagerAdapter;
+import org.secuso.privacyfriendlyfoodtracker.activities.helper.BaseActivity;
+
+import java.util.Date;
+
+/**
+ * Base code for tapped activity.
+ *
+ * @author Simon Reinkemeier
+ */
+public class BaseAddFoodActivity extends AppCompatActivity {
+
+    Date date;
+    String name;
+    int calories;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_base_add_food);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_food);
+        AddFoodPagerAdapter myPagerAdapter = new AddFoodPagerAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(myPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout_food);
+        tabLayout.setupWithViewPager(viewPager);
+
+        Intent intent = getIntent();
+
+
+        long dateLong = intent.getLongExtra("DATE", System.currentTimeMillis());
+        date = new Date();
+        date.setTime(dateLong);
+    }
+
+    protected int getNavigationDrawerID() {
+        return R.id.nav_statistic;
+    }
+
+}
---
+++ app/src/main/java/org/secuso/privacyfriendlyfoodtracker/activities/SearchFoodFragment.java
@@ -0,0 +1,65 @@
+package org.secuso.privacyfriendlyfoodtracker.activities;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import org.secuso.privacyfriendlyfoodtracker.R;
+import org.secuso.privacyfriendlyfoodtracker.activities.adapter.DatabaseFacade;
+import org.secuso.privacyfriendlyfoodtracker.activities.helper.DateHelper;
+import org.secuso.privacyfriendlyfoodtracker.database.ApplicationDatabase;
+import org.secuso.privacyfriendlyfoodtracker.database.ConsumedEntrieAndProductDao;
+import org.secuso.privacyfriendlyfoodtracker.viewmodels.SharedStatisticViewModel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass. Contains a possibility to search for a product.
+ *
+ * @author Simon Reinkemeier
+ */
+public class SearchFoodFragment extends Fragment {
+    SharedStatisticViewModel sharedStatisticViewModel;
+    Activity referenceActivity;
+    View parentHolder;
+    TextView textView;
+    DatabaseFacade databaseFacade;
+
+    public SearchFoodFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        referenceActivity = getActivity();
+        parentHolder = inflater.inflate(R.layout.content_search, container, false);
+        sharedStatisticViewModel = ViewModelProviders.of(getActivity()).get(SharedStatisticViewModel.class);
+        try {
+            databaseFacade = new DatabaseFacade(referenceActivity.getApplicationContext());
+        } catch (Exception e){
+            Log.e("Error", e.getMessage());
+        }
+
+        return parentHolder;
+    }
+
+
+}
